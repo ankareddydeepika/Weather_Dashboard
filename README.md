119263ddb61bd3779301bcf5ba4eb042b307b7c6
Deepika Ankareddi <ankareddydeepika@gmail.com>
2020-04-11 18:43:19 -0400

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Weather_Dashboard
+
+Created a the weather outlook for multiple cities so that user can plan a trip accordingly
+
+## Criteria
+
+When user search for a city
+User is presented with current and future conditions for that city and that city is added to the search history
+When user view current weather conditions for that city
+User is presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+Then user is also presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+When user open the weather dashboard then he is presented with the last searched city forecast
+
+## To achive the above criteria
+
+1. Created layout for the page using rows, coloumns and cards using Jquery and Bootstrap.
+2. Created a function to populate previous searches while clicking search icon
+3. Created a function to Coverte date.
+4. Used three ajax calls to populate the weather data
+
+API used: http://api.openweathermap.org/data/2.5/forecast?
+         https://api.openweathermap.org/data/2.5/weather?
+         http://api.openweathermap.org/data/2.5/uvi?
+
+Application Link:  
+
+![weatherdashboard](./Assets/Weather_dashboard.gif)
--- script.js
+++ script.js
@@ -6,15 +6,20 @@ if (a !== "") {
     search(a)
 }
 //populate prevous searches
-function populatePreviousSearches(){
-    if (savedSearches.length === 9) {
-        savedSearches.shift()
+function populatePreviousSearches() {
+    if(savedSearches.includes($("#search-term").val().trim())){
+        
+    } else {
+        if (savedSearches.length === 9) {
+            savedSearches.shift()
+        }
+        savedSearches.push($("#search-term").val().trim());
     }
-    savedSearches.push($("#search-term").val().trim());
-    for(i=0; i<savedSearches.length; i++){
+  
+    for (i = 0; i < savedSearches.length; i++) {
 
         $("#previous-searches").prepend("<br><hr>" + savedSearches[i]);
-    } 
+    }
 }
 function clear() {
     $("#previous-searches").empty()
@@ -30,7 +35,7 @@ function buildQueryUrl(searchterm) {
     var queryParameters = { "appid": "0a0a4078e577711da637196946879a75" };
 
     queryParameters.q = searchterm
-    queryParameters.units="imperial"
+    queryParameters.units = "imperial"
     //   console.log(queryUrl + $.param(queryParameters));
     return (queryUrl + $.param(queryParameters));
 }
@@ -44,7 +49,7 @@ function buildForecastUrl(searchterm) {
     var queryParameters = { "appid": "0a0a4078e577711da637196946879a75" };
     console.log(searchterm)
     queryParameters.q = searchterm
-    queryParameters.units="imperial"
+    queryParameters.units = "imperial"
 
     return (queryUrl + $.param(queryParameters));
 
@@ -52,16 +57,16 @@ function buildForecastUrl(searchterm) {
 
 //UV URL
 
-function buildUVurl(lati,long){
+function buildUVurl(lati, long) {
 
     var queryUrl = "http://api.openweathermap.org/data/2.5/uvi?";
 
-    var queryParameters = {"appid":"0a0a4078e577711da637196946879a75"};
+    var queryParameters = { "appid": "0a0a4078e577711da637196946879a75" };
 
     queryParameters.lat = lati
     queryParameters.lon = long
 
-    return(queryUrl + $.param(queryParameters));
+    return (queryUrl + $.param(queryParameters));
 }
 
 
@@ -76,13 +81,13 @@ function convertDate(unixtimestamp) {
 
 }
 // Creating function to search, while clicking search icon
-function search(searchterm){
+function search(searchterm) {
     clear()
-    
+
     localStorage.setItem("lastsearcheditem", searchterm)
     var weatherUrl = buildQueryUrl(searchterm);
     var forecasteUrl = buildForecastUrl(searchterm);
-    
+
     console.log(weatherUrl);
     console.log(forecasteUrl);
 
@@ -90,12 +95,12 @@ function search(searchterm){
 
 
 
-//Current weather forecaste
+    //Current weather forecaste
     $.ajax({
         url: weatherUrl,
         method: "GET"
     }).then(response => {
-console.log(response)
+        console.log(response)
         $("#Weather-forecaste").prepend(`
         <h1>${response.name} (${convertDate(response.dt)})<h1/>
         <p style="font-size:20px">Temperature: ${response.main.temp}<p/>
@@ -108,29 +113,29 @@ console.log(response)
         $.ajax({
             url: UVUrl,
             method: "GET"
-        }).then(response =>{
-    
+        }).then(response => {
+
             var uvValue = response.value
 
             var background;
 
-            if(uvValue === 3 || uvValue <= 5){
+            if (uvValue === 3 || uvValue <= 5) {
 
                 background = "yellow";
             }
-            else if(uvValue === 6 || uvValue <= 7){
+            else if (uvValue === 6 || uvValue <= 7) {
 
                 background = "orange";
             }
-            else if(uvValue === 8 || uvValue <= 10){
+            else if (uvValue === 8 || uvValue <= 10) {
 
                 background = "red";
             }
-            else{
+            else {
 
                 background = "violet";
             }
-            
+
             $("#Weather-forecaste").append(`
             <div class="card" style="width: 8rem" >
             <div class="card-body"  style="background-color: ${background}"><p Style="font-size:20px">UV: ${uvValue} <p/>
@@ -138,35 +143,36 @@ console.log(response)
             </div>
             `);
 
-            
+
         })
 
     })
-   
 
 
-//Five day forecaste
+
+    //Five day forecaste
     $.ajax({
         url: forecasteUrl,
         method: "GET"
-    }).then(response =>{
+    }).then(response => {
 
         console.log(response);
 
-        for(i=0; i<response.cnt; i++){
+        for (i = 0; i < response.cnt; i++) {
 
             var weather = response.list[i];
             var str = weather.dt_txt;
             var substr = "12:00:00";
 
-            if(str.includes(substr)){
+            if (str.includes(substr)) {
 
                 $("#fivedayforecaste").append(`
                 <div class="card"><div/>
-                <div class="card-body">
+                <div class="card-body" style="background-color:grey">
                 <h4>${convertDate(weather.dt)}<h4/>
+                <img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png"></img>
                 <p style="font-size:20px">Temperature: ${weather.main.temp}<p/>
-        <p style="font-size:20px">Humidity: ${weather.main.humidity}%<p/>
+                <p style="font-size:20px">Humidity: ${weather.main.humidity}%<p/>
                 </div>             
                 `)
             }
@@ -180,8 +186,8 @@ console.log(response)
 
 
 $("#searchbtn").on("click", function () {
-    
-search($("#search-term").val().trim())
+
+    search($("#search-term").val().trim())
 
 
 });
